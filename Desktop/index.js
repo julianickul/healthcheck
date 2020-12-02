5dad4dbbfee830b07bc156c24ad986b87dc57c8a
Yuliya <julpostbox@mail.ru>
2020-12-02 22:46:37 +0300

    Updates

---
+++ Desktop/index.js
@@ -0,0 +1,13 @@
+
+/* ---------- JQUERY --------- */
+/*import jQuery from "jquery";
+window.$ = window.jQuery = jQuery;*/
+
+/* ---------- Bootstrap Optional --------- */
+/*import 'bootstrap/js/dist/util';
+import 'bootstrap/js/dist/button';
+import 'bootstrap/js/dist/tab';*/
+
+/* ---------- Toggle Main Nav --------- */
+/*import {initTogglePageView} from '../../assets/src/js/lib';
+initTogglePageView();*/
